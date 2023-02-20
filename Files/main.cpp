be9162868524346a91a40d3fc2ca85b62abbeee3
RogerK0bs <107825336+RogerK0bs@users.noreply.github.com>
2023-02-21 01:24:35 +0700

    File_new

--- Files/main.cpp
+++ Files/main.cpp
@@ -1,8 +1,48 @@
 #include<iostream>
+#include<fstream>
+#include<Windows.h>
 using namespace std;
-
+#define WRITE_TO_FILE
+//#define READ_FROM_FILE
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Files" << endl;
+#ifdef WRITE_TO_FILE
+	char filename[_MAX_FNAME] = {};
+	cout << "Введите имя файла: "; 
+	cin.getline(filename, _MAX_FNAME);
+	if (strcmp(filename + strlen(filename) - 4, ".txt"))strcat_s(filename, _MAX_FNAME, ".txt");
+	ofstream fout;								//1 Создаем поток 
+	fout.open(filename, std::ios_base::app);	//2 открываем поток
+	fout << "Hellow File:"<< endl;				//3 Пишем в поток
+	fout.close();								//4 Закрыть поток
+	char sz_command[_MAX_FNAME] = "notepad ";
+	strcat_s(sz_command,_MAX_FNAME, filename);
+	system(sz_command);
+
+#endif // WRITE_TO_FILE
+#ifdef READ_FROM_FILE
+	ifstream fin;
+	fin.open("File.txt");
+	if (fin.is_open())
+	{
+		const int SIZE = 5256;
+		char sz_buffer[SIZE]{};
+		while (!fin.eof())
+		{
+			//fin >> sz_buffer;
+			fin.getline(sz_buffer, SIZE);
+			cout << sz_buffer << endl;
+		}
+		fin.close();
+	}
+	else
+	{
+		std::cerr << "Error: File not found" << endl;
+	}
+#endif // READ_FROM_FILE
+
+
+
+
 }
