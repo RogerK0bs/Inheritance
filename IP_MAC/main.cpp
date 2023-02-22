c7fb9aa0430701b280ebea9a95aee85805aff2e0
RogerK0bs <107825336+RogerK0bs@users.noreply.github.com>
2023-02-23 00:18:04 +0700

    Rabotaet xD

--- IP_MAC/main.cpp
+++ IP_MAC/main.cpp
@@ -2,43 +2,47 @@
 
 void main()
 {
-	const int SIZE = 1024;
-	
-	char symbol = ' ';
+	const int colums = 40, str = 12;
+	char mass_buffer[colums][str]{};
+	char end = '\n';
 	ofstream fout;
-	fout.open("201 ready.txt");
+	fout.open("201 ready.txt", std::ios_base::app);
 	ifstream fin;
 	fin.open("201 RAW.txt");
 	if (fin.is_open())
 	{
-		char sz_buffer[SIZE]{};
-		while (!fin.eof())
+		for (size_t i = 0; i < str; i++)
 		{
-			fin.getline(sz_buffer, SIZE);
-			cout << sz_buffer << endl;
+			fin.getline(mass_buffer[i], colums, end);
+			//cout << mass_buffer[i] << endl;
+			char* mac_copy=new char[colums]{};
+			char* ip_copy=new char[colums]{};
+			for (size_t j = 0; j < colums - 22; j++)
+			{
+				mac_copy[j] = mass_buffer[i][j + 22];
+			}
+			for (size_t j = 0; j < 15; j++)
+			{
+				ip_copy[j] = mass_buffer[i][j];
+			}
+			for (size_t j = 0; j < colums; j++)
+			{
+				if (j < 18)fout << mac_copy[j];
+				else fout << ' ';
+				if (j == 26)
+				{
+					for (size_t er = 0; er < 20; er++)
+					{
+						fout << ip_copy[er];	
+					}
+				}
+			}fout << "\n";
 		}
 	}
 	else
 	{
 		std::cerr << "Error: File not found" << endl;
 	}
-	for (size_t i = 0; i < 12; i++)
-	{
-		
-		while (!fin.eof())
-		{
-			char* buffer;
-			if (fin.eof() != symbol)
-			{
-				fin.getline(buffer, SIZE);
-				cout << buffer << "\t";
-			}
-		}
-	}
-
-	
-
-
-	fout.close();
 	fin.close();
+	fout.close();
 }
